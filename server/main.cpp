e6ea55f99485baa71cd453ca73ffd1d76273b319
balegajay <balegajay@gmail.com>
2024-01-23 19:10:29 +0100

    move server contents to a new folder

--- server/main.cpp
+++ server/main.cpp
@@ -17,18 +17,18 @@ int main(int argc, char *argv[]) {
 #endif
 
   // create the inferencer
-  std::unique_ptr<IInference> inferencer =
-      std::make_unique<TorchInference>("best.torchscript", "");
+  // std::unique_ptr<IInference> inferencer =
+  //     std::make_unique<TorchInference>("best.torchscript", "");
 
   // create the streamer to get images from the clients
   Streamer streamer(argc, argv, 5000, 1);
   streamer.Setup();
 
   // connect to the new image signal from the client
-  streamer.new_img.connect([&inferencer](cv::Mat img) {
+  streamer.new_img.connect([/*&inferencer*/](cv::Mat img) {
     // get the model results for the image (model inferencing has a lot of
     // errors)
-    inferencer->Infer(img);
+    // inferencer->Infer(img);
     // TODO use the results in an alert system
   });
 
