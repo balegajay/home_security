af11d62c76a80806e435f3f20d2509635e14d494
Anashku <balegajay@gmail.com>
2024-12-10 07:54:02 +0000

    cleanup code

--- server/communication/streamer/streamer.hpp
+++ server/communication/streamer/streamer.hpp
@@ -2,7 +2,6 @@
 
 #include <functional>
 #include <memory>
-#include <opencv2/core.hpp>
 #include <boost/signals2/signal.hpp>
 
 class Impl;
@@ -15,8 +14,7 @@ class Streamer {
  public:
   Streamer(int argc, char* argv[], int video_port, int audio_port);
   ~Streamer();
-  bool Setup();
+  bool Setup(std::string pipeline);
   bool Pause();
   void Start();
-  boost::signals2::signal<void(cv::Mat)> new_img;
 };
